72b2a62289ec16e72f4fd4a33d36467fea6b8026
Sukruth M K <suk_mallur@yahoo.co.in>
2020-10-23 13:35:44 -0700

    removed unused variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import CreateJournal from "./components/CreateJournal";
